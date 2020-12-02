fd60d69f95bdf4b725502602a430e17f73084f2f
Dream4ever <cnhwvista@gmail.com>
2020-12-02 23:33:04 +0800

    改进脚本输出时的颜色

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
+# Define colors
+color_print () {
+  BLUE_COLOR="96m";
+
+  STARTCOLOR="\e[$BLUE_COLOR";
+  ENDCOLOR="\e[0m"; # white
+
+  printf "$STARTCOLOR%b$ENDCOLOR" "$1";
+}
+
 # If a command fails then the deploy stops
 set -e
 
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+color_print "Deploying updates to GitHub...\n\n" "info";
 
+color_print "Set proxy for GitHub...\n\n" "info";
 export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
 
 # Build the project
@@ -13,11 +24,13 @@ hugo -D
 # Go To Public folder
 cd public
 
+color_print "\nPush generated static blog files...\n" "info";
 # Push generated static blog files
 git push
 
 # Go to root directory
 cd ..
 
+color_print "\nPush source filess...\n" "info";
 # Push source filess
 git push
