a4366221e0bb2b4ff251963d513357a7e1a80623
Samsara9527 <cnhwvista@gmail.com>
2022-07-09 00:05:10 +0800

    config: 博客首页默认重定向至笔记首页

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,6 +37,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          routeBasePath: '/', // Serve the docs at the site's root
           sidebarPath: require.resolve('./sidebars.js'),
         },
         blog: {
